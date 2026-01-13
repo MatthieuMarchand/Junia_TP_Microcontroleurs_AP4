f126ec1d7393679fde81c24dc8e13f5b51f42bfd
Arthaud <arthaudp33@gmail.com>
2026-01-13 13:44:10 +0100

    I.a Resultats obtenus

--- TP1a_helloworld.X/main.c
+++ TP1a_helloworld.X/main.c
@@ -1,4 +1,7 @@
-// Résultats obtenus : les 4 premières et dernières leds s'allument à un intervalle d'environ 1s
+/** 
+Résultats obtenus : 
+Les 4 premières et dernières leds s'allument à un intervalle d'environ 1s
+*/ 
 
 #include "../Ressources/configbits.h" // Bits de configuration
 #include <xc.h>         // Definition des registres specifiques au uC
@@ -6,17 +9,23 @@
 // Allume les LEDs D1-8 de la carte Microchip Explorer 8 
 // MCU : PIC16F1719 
 //
-// | LED | Pin |
-// |-----|-----|
-// |  D1 | R5 |
-// |  D2 | R6 |
-// |  D3 | R7 |
-// |  D4 | R8 |
-// |  D5 | R9 |
-// |  D6 | R10 |
-// |  D7 | R11 |
-// |  D8 | R12 |
+// MCU : PIC16F1719 
 //
+// | LED   | Registre |
+// |-------|----------|
+// | D1-D4 | LATD     |
+// | D5-D8 | LATB     |
+//
+// | LED | Registre       | Pin |
+// |-----|----------------|-----|
+// | D1  | LATDbits.LATD0 | R5  |
+// | D2  | LATDbits.LATD1 | R6  |
+// | D3  | LATDbits.LATD2 | R7  |
+// | D4  | LATDbits.LATD3 | R8  |
+// | D5  | LATBbits.LATB0 | R9  |
+// | D6  | LATBbits.LATB1 | R10 |
+// | D7  | LATBbits.LATB2 | R11 |
+// | D8  | LATBbits.LATB3 | R12 |
 
 void allumer_que_d1_a_d4(void);
 void allumer_que_d5_a_d8(void);
@@ -34,8 +43,9 @@ void main(void) {
     };
 }
 
+// Attendre approximativement 1s
 void delai_approx(void) {
-    for (long i = 0; i < 90000; i++);
+    for (long i = 0; i < 90000; i++); // Obtenu expérimentalement
 }
 
 void allumer_que_d1_a_d4(void) {
